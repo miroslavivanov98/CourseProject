c9334fbb898aa6f3ec1a57455c5309bb614c69da
miroslavivanov98 <32927661+miroslavivanov98@users.noreply.github.com>
2018-05-11 19:55:49 +0300

    Add Entity and HTML

---
+++ CourseProject.DB/Entities/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CourseProject.DB.Entities
+{
+    class User : BaseEntity
+    {
+        [Required, StringLength(20)]
+        public string Email { get; set; }
+
+        [Required, StringLength(20)]
+        public string PassHash { get; set; }
+
+        [Required, StringLength(20)]
+        public bool IsAdmin { get; set; }
+
+        [Required, StringLength(20)]
+        public bool IsBanned { get; set; }
+
+    }
+}
---
+++ CourseProject.DB/Entities/Weapons.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CourseProject.DB.Entities
+{
+    class Weapons : BaseEntity
+    {
+        [Required, StringLength(50)]
+        [Index("IX_WeaponsNameUnique", IsUnique = true)]
+        public string Name { get; set; }
+
+        [Required]
+        public int Caliber { get; set; }
+
+        [Required, StringLength(20)]
+        public string Weight { get; set; }
+
+        [Required, StringLength(20)]
+        public string BarrelLength { get; set; }
+
+        [Required, StringLength(20)]
+        public string Price { get; set; }
+
+
+    }
+}
