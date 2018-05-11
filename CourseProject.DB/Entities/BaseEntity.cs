c2064ad21f22b4c95b8d9756eec83d69df3733c6
miroslavivanov98 <32927661+miroslavivanov98@users.noreply.github.com>
2018-05-11 19:55:49 +0300

    Migrations

---
+++ CourseProject.DB/Entities/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CourseProject.DB.Entities
+{
+    class BaseEntity
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int Id { get; set; }
+    }
+}
---
+++ CourseProject.DB/Entities/Manafacturer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CourseProject.DB.Entities
+{
+    class Manafacturer : BaseEntity
+    {
+        [Required, StringLength(20)]
+        public string Name { get; set; }
+
+        [Required, StringLength(20)]
+        public string Address { get; set; }
+
+        [Required, StringLength(20)]
+        public string Phone { get; set; }
+    }
+}
